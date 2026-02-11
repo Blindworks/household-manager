2cb92615f6fa6ee67fd5b7b20e3cfc7da85d6ac8
Benedikt Lind <benedikt.lind@lhind.dlh.de>
2026-02-11 17:07:45 +0100

    Logging added

--- backend/src/main/java/com/household/manager/tapo/TapoController.java
+++ backend/src/main/java/com/household/manager/tapo/TapoController.java
@@ -2,6 +2,7 @@ package com.household.manager.tapo;
 
 import com.household.manager.tapo.dto.TapoDiscoveryDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/tapo")
 @RequiredArgsConstructor
+@Slf4j
 public class TapoController {
 
     private final TapoDiscoveryService tapoDiscoveryService;
 
     @GetMapping("/discover")
     public ResponseEntity<List<TapoDiscoveryDto>> discover() {
-        return ResponseEntity.ok(tapoDiscoveryService.discoverTapoDevices());
+        log.info("Received Tapo discovery request on /api/tapo/discover");
+        List<TapoDiscoveryDto> devices = tapoDiscoveryService.discoverTapoDevices();
+        log.info("Tapo discovery completed with {} device(s)", devices.size());
+        return ResponseEntity.ok(devices);
     }
 }
